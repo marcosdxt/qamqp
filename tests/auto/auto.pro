d16387e90872b6e2075e6d6b04679b4b460a49ba
= <marcos@marcosaquino.com>
2023-02-04 12:05:08 -0300

    limpeza do diretório

--- tests/auto/auto.pro
+++
@@ -1,6 +0,0 @@
-TEMPLATE = subdirs
-SUBDIRS = \
-    qamqpclient \
-    qamqpexchange \
-    qamqpqueue \
-    qamqpchannel
--- tests/gen-coverage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-lcov --capture --directory . --output-file coverage-gcov.info --no-external
-lcov --output-file coverage-gcov.info --remove coverage-gcov.info 'moc_*.cpp' '*.moc*' '.*rcc*' '*3rdparty*'
-genhtml coverage-gcov.info --output-directory doc/coverage
